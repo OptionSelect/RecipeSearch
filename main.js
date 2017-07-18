0025b7c33453b2f2df8b5ccfc0c59438617da302
Brandyn Sullins <woofenwauzer@gmail.com>
2017-07-18 16:48:23 -0400

    new colors

--- main.js
+++ main.js
@@ -16,17 +16,15 @@ go.addEventListener('click', () => {
     for (var i = 0; i < data.results.length; i++) {
       let newli = document.createElement('li')
 
+      if (data.results[i].thumbnail === '') {
+        newli.style.backgroundImage = `url('http://via.placeholder.com/200x200')`
+      } else {
+        newli.style.backgroundImage = `url(${data.results[i].thumbnail})`
+      }
+
       let text = document.createElement('h3')
       text.textContent = data.results[i].title
       newli.appendChild(text)
-
-      let image = document.createElement('img')
-      if (data.results[i].thumbnail === '') {
-        image.setAttribute('src', 'http://via.placeholder.com/200x200')
-      } else {
-        image.src = data.results[i].thumbnail
-      }
-      newli.appendChild(image)
 
       let hacheref = document.createElement('a')
       hacheref.href = data.results[i].href
